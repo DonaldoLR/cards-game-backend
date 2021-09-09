32ccdca8b4f2902d20ac566c27709a31d5090c4e
github username <github email address>
2021-09-09 15:50:25 -0400

    created database, ran migrations and seed file, created user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Starting Seed File"
+
+puts "-- Creating Users --"
+User.create!(name: "Donaldo", avatar_img: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Brown")
+User.create!(name: "John", avatar_img: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light")
+
+puts "-- 👨‍💻 Users have been created --"
