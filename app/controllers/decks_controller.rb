b45e72a5f53f3b72af09a2817ada18ed36d01dcd
github username <github email address>
2021-09-11 02:47:02 -0400

    added functionality to add a card to a user via deck resource

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,12 @@
 class DecksController < ApplicationController
+    def create
+        deck = Deck.create!(deck_params)
+        render json: deck, status: :created
+    end
+
+    private 
+
+    def deck_params 
+        params.permit(:user_id, :card_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # resources :decks
+  resources :decks, only: [:create, :destroy]
   # resources :cards
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
